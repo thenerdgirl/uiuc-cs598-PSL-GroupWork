0a3de1ce9deb44eea5d78e0c3b2da6a46e5f4fbd
mich1eal <msmille3@gmail.com>
2023-09-17 13:22:13 -0600

    Added empty file for code

---
+++ projects/project1/mymain.R
@@ -0,0 +1,25 @@
+# Project 1
+# CS598 Practical Statistical Learning
+# Naomi Brhagat - nbhgat3
+# Michael Miller - msmille3
+# Joe May - jemay3
+# 9/17/2023
+
+# Campuswire post: https://campuswire.com/c/G06C55090/feed/193
+# PDF instructions https://liangfgithub.github.io/Proj/F23_Proj1.pdf
+
+
+
+# Instructions
+# Code: Your R/Python script should be in a singular file named either mymain.R or mymain.py.
+# This script should:
+#   – Accept train.csv and test.csv as inputs.
+# – Generate two submission files based on the specified format (detailed below).
+# – Important: Do not submit ZIP files or markdown/notebook files.
+# The structure of your mymain.R (or mymain.py) should adhere to the following:
+#   – Step 0: Load necessary R libraries or Python packages
+# – Step 1: Preprocess the training data, then fit the two models.
+# ∗ Note: At this step, you are strictly not allowed to access the test data.
+# – Step 2: Preprocess test data, then save predictions into two files: mysubmission1.txt and
+# mysubmission2.txt. (The specific format for these files is detailed below.)
+# ∗ Note: At this step, you are strictly not allowed to access the training data
