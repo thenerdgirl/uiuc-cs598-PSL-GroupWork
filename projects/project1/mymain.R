e3b1208853bb975fbce0a8860ba16c46d96247bf
mich1eal <msmille3@gmail.com>
2023-09-18 23:56:18 -0600

    Added read/write to script

--- projects/project1/mymain.R
+++ projects/project1/mymain.R
@@ -8,21 +8,13 @@
 # Campuswire post: https://campuswire.com/c/G06C55090/feed/193
 # PDF instructions https://liangfgithub.github.io/Proj/F23_Proj1.pdf
 
+# TODO set this to FALSE before submitting 
+DEBUG = TRUE 
+if (DEBUG) {print('Running in debug mode! Disable before submitting!')}
 
+# use this to select which fold to use (debug only)
+fold_num = 1
 
-# Instructions
-# Code: Your R/Python script should be in a singular file named either mymain.R or mymain.py.
-# This script should:
-#   – Accept train.csv and test.csv as inputs.
-# – Generate two submission files based on the specified format (detailed below).
-# – Important: Do not submit ZIP files or markdown/notebook files.
-# The structure of your mymain.R (or mymain.py) should adhere to the following:
-#   – Step 0: Load necessary R libraries or Python packages
-# – Step 1: Preprocess the training data, then fit the two models.
-# ∗ Note: At this step, you are strictly not allowed to access the test data.
-# – Step 2: Preprocess test data, then save predictions into two files: mysubmission1.txt and
-# mysubmission2.txt. (The specific format for these files is detailed below.)
-# ∗ Note: At this step, you are strictly not allowed to access the training data
 
 clean_data = function(in_df) { 
   # in_df - dataframe matching format of Ames housing dataset
@@ -34,6 +26,43 @@ clean_data = function(in_df) {
   
   return(out_df)
   }
+
+print_formatted = function(df, file=file_name) {
+  # format output per guidance 
+  write.table(df,
+              file=file, 
+              row.names=FALSE, 
+              quote=FALSE, 
+              sep = ",  ")
+  }
+
+# load data
+if(DEBUG) {
+  file_dir =  paste0('fold', as.character(fold_num))
+  test_x = read.csv(paste0(file_dir, '/test.csv'))
+  test_y = read.csv(paste0(file_dir, '/test_y.csv'))
+  train = read.csv(paste0(file_dir, '/train.csv'))
+} else {
+  test_x = read.csv(paste0('test.csv'))
+  train = read.csv(paste0('train.csv'))
+}
+  
+# clean data
+train = clean_data(train)
+test_x = clean_data(test_x)
+
+# train models
+
+# get predictions
+pred1 = test_y
+pred2 = test_y
+
+# evaluate 
+
+# write output
+print_formatted(pred1, file='mysubmission1.txt')
+write.csv(pred2, file='mysubmission2.txt')
+
 
 
 
