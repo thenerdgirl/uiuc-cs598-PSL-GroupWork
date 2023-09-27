7c04768d49ace34feb42e1e0419051ee5e70063a
mich1eal <msmille3@gmail.com>
2023-09-26 21:35:34 -0600

    results are right on border of good enough

--- projects/project1/mymain.R
+++ projects/project1/mymain.R
@@ -44,6 +44,9 @@ clean_input = function(df_in) {
   # zero out the Garage Year Built missing data
   df_in$Garage_Yr_Blt[is.na(df_in$Garage_Yr_Blt)] = 0
   
+  # remove weak variables 
+  df_in = df_in[ , -which(names(df_in) %in% vars_to_remove)]
+  
   # 95% winsorization
   for(var in winsor_vars){
     tmp = df_in[ , var]
@@ -51,57 +54,50 @@ clean_input = function(df_in) {
     tmp[tmp > quan_val] = quan_val
     df_in[ , var] = tmp
   }
-
-  # handle nominals 
-  # iterate through the nominals we listed out
-  for (i in 1:nrow(nominal_df)) {
-    # we need a new column for each value in each nominal
-    row = nominal_df[i, ]
-    name = row$name
-    temp_columns = matrix(0, nrow(df_in), length(row$options[[1]]))
-    col_names = vector('character', length=length(row$options[[1]]))
-    
-    # iterate through options 
-    for (j in 1:length(row$options[[1]])) {
-      option = row$options[[1]][j]
-      
-      # temp column should be true if the value is equal to this options
-      temp_columns[df_in[, name]==option, j] = 1
-      col_names[j] = paste(name, '_', option, sep='')
-    }
-    
-    colnames(temp_columns) = col_names
-    df_in = cbind(df_in, temp_columns)
-  }
   
-  numeric_columns = sapply(df_in, function(x) is.integer(x) || is.numeric(x))
-  out_df = df_in[, numeric_columns]
+  # Convert categorical to 1-hot vectors 
+  categorical_vars = colnames(df_in)[which(sapply(df_in, function(x) mode(x)=="character"))]
+  df_out = df_in[, !colnames(df_in) %in% categorical_vars, drop=FALSE]
+  n_train = nrow(df_out)
+  
+  for(var in categorical_vars){
+    mylevels = sort(unique(df_in[, var]))
+    m = length(mylevels)
+    m = ifelse(m>2, m, 1)
+    tmp.train = matrix(0, n_train, m)
+    col.names = NULL
+    for(j in 1:m){
+      tmp.train[df_in[, var]==mylevels[j], j] = 1
+      col.names = c(col.names, paste(var, '_', mylevels[j], sep=''))
+    }
+    colnames(tmp.train) = col.names
+    df_out = cbind(df_out, tmp.train)
+  }
   
   # if there is Sale_Price , put it back at the end
-  if ("Sale_Price" %in% names(out_df)) {
-    order = c(names(out_df)[names(out_df) != "Sale_Price"], "Sale_Price")
-    out_df = out_df[, order]
+  if ("Sale_Price" %in% names(df_out)) {
+    order = c(names(df_out)[names(df_out) != "Sale_Price"], "Sale_Price")
+    df_out = df_out[, order]
   }
-  return(out_df)
+  return(df_out)
 }
 
-force_col_match(source, target) {
+force_col_match = function(source, target) {
   # forces target df to have the same columns and column order as source
   # for missing columns creates columns of 0s 
   
   # get missing columns
-  missing_cols = setdiff(colnames(target), colnames(source))
+  missing_cols = setdiff(colnames(source), colnames(target))
   
   # make zero columns for the missing columns
   missing_df = data.frame(matrix(0, nrow=nrow(target), ncol=length(missing_cols)))
-  colnames(missing_df) = missing_col
+  colnames(missing_df) = missing_cols
   
   # add missing columns to target
-  target = cbind(target, temp_columns)
+  target = cbind(target, missing_df)
   
   # drop extra columns and put columns in order
   correct_cols = colnames(source)
-  
   target = target[, correct_cols]
   
   return(target)
@@ -133,21 +129,28 @@ get_rmse = function(y_pred, y_actual) {
   return(total)
 }
 
-train_and_eval = function(test_x, train, test_y) {
+train_and_eval = function(test_x_raw, train_raw, test_y) {
   # if train_y is not null, prints metrics to screen
   # if train_y is null. does not evaluate and instead prints output to file
   
-  ############ LINEAR MODEL  ############
-  train_linear = clean_input(train)
-  test_x_linear = clean_linear(test_x)
-  test_idx_linear = test_x_linear$PID
+  ############ CLEAN INPUTS   ############
+  # pass through preprocessing
+  train = clean_input(train_raw)
+  test_x = clean_input(test_x_raw)
   
-  # inputs need to be matrices 
-  train_x_mat = as.matrix(train_linear[ ,2:(ncol(train_linear)-1)]) # omit PID and price
+  # test must match train exactly (be aware this will add PID column!)
+  test = force_col_match(train, test_x)
+  
+  # get PIDS in order to get correct Ys later
+  test_idx = test$PID
+  
+  # get data in format of matrices
+  train_x_mat = as.matrix(train[ ,2:(ncol(train)-1)]) # omit PID and price
   # we are training model to predict LOG of sale price
-  train_y = log(train_linear[, ncol(train_linear)]) # price only
-  test_x_mat = as.matrix(test_x_linear[ ,2:ncol(test_x_linear)]) # omit PID
+  train_y = log(train[, ncol(train)]) # price only
+  test_x_mat = as.matrix(test[ ,2:(ncol(test)-1)]) # omit PID and price
   
+  ############ LINEAR MODEL  ############
   # temp model to get optimal <something>
   start_linear = Sys.time()
   temp_model = cv.glmnet(train_x_mat, train_y, alpha=1)
@@ -163,15 +166,6 @@ train_and_eval = function(test_x, train, test_y) {
   y_linear = as.vector(predict(model_linear, s=model_linear$lambda.min, newx=test_x_mat[, selected_vars]))
   
   ############ TREE MODEL  ############   
-  train_tree = clean_tree(train)
-  test_x_tree = clean_tree(test_x)
-  test_idx_tree = test_x_tree$PID
-  
-  train_x_mat = as.matrix(train_tree[ ,2:(ncol(train_tree)-1)]) # omit PID and price
-  # we are training model to predict LOG of sale price
-  train_y = log(train_tree[, ncol(train_tree)]) # price only
-  test_x_mat = as.matrix(test_x_tree[ ,2:ncol(test_x_tree)]) # omit PID
-  
   # train and time tree model
   start_tree = Sys.time()
   model_tree = xgboost(data = train_x_mat,
@@ -190,18 +184,17 @@ train_and_eval = function(test_x, train, test_y) {
   #######  evaluate 
   if(is.null(test_y)) {
     # print output to file 
-    print_formatted(exp(y_linear), test_idx_linear, 'mysubmission1.txt')
-    print_formatted(exp(y_tree), test_idx_tree, 'mysubmission2.txt')
+    print_formatted(exp(y_linear), test_idx, 'mysubmission1.txt')
+    print_formatted(exp(y_tree), test_idx, 'mysubmission2.txt')
   } else {
     # print metrics 
     test_y$Sale_Price = log(test_y$Sale_Price)
     
     # first, only get the y values we actually used
-    y_actual_linear = subset(test_y, PID %in% test_idx_linear)$Sale_Price
-    y_actual_tree = subset(test_y, PID %in% test_idx_tree)$Sale_Price
+    y_actual = subset(test_y, PID %in% test_idx)$Sale_Price
 
-    rmse_linear = get_rmse(y_linear, y_actual_linear)
-    rmse_tree = get_rmse(y_tree, y_actual_tree)
+    rmse_linear = get_rmse(y_linear, y_actual)
+    rmse_tree = get_rmse(y_tree, y_actual)
     
     cat(sprintf('%d\t%.3f\t%.3f\t%.3f\t%.3f\t\n',
                 fold_num, 
@@ -224,10 +217,10 @@ if(DEBUG) {
   # evaluate all folds 
   for (fold_num in 1:10) {
     file_dir =  paste0('fold', as.character(fold_num))
-    test_x = read.csv(paste0(file_dir, '/test.csv'))
+    test_x_raw = read.csv(paste0(file_dir, '/test.csv'))
     test_y = read.csv(paste0(file_dir, '/test_y.csv'))
-    train = read.csv(paste0(file_dir, '/train.csv'))
-    train_and_eval(test_x, train, test_y)
+    train_raw = read.csv(paste0(file_dir, '/train.csv'))
+    train_and_eval(test_x_raw, train_raw, test_y)
   } 
 } else {
   # only evaluate data in current directory 
