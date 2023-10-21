3e33215ac452c56a67811d5caf7ad262b11c31b9
jmayjmay <112908616+jmayjmay@users.noreply.github.com>
2023-10-21 16:40:03 -0500

    added functions to get deparment svd, reshape matrix, merge to data set, and get predictions. Loop can't proceed because of a bug related to mismatching dimension shapes

--- projects/project2/mymain.R
+++ projects/project2/mymain.R
@@ -24,6 +24,11 @@ DEBUG = TRUE
 
 set.seed(235)
 
+get_depts = function(train_data){
+  depts = unique(train_data$Dept)
+  return(depts)
+}
+
 department_matrix = function(train_data, d){
   matrix = train_data %>%
     filter(Dept == d) %>%
@@ -37,10 +42,31 @@ department_matrix = function(train_data, d){
   return(matrix)
 }
 
-get_depts = function(train_data){
-  depts = unique(train_data$Dept)
-  return(depts)
+dept_svd = function(X){
+  d = min(length(X)-1, nrow(X), 8)
+  cat(length(X)-1, nrow(X),d)
+  X[is.na(X)] = 0
+  store_mean = rowMeans(t(X[,-1]))
+  X_less_mean = t(X[,-1]) - store_mean
+  
+  svd_decom = svd(X_less_mean)
+  U = svd_decom$u[,1:d]
+  D = diag(svd_decom$d[1:d])
+  Vt = t(svd_decom$v[,1:d])
+  
+  X_s = U %*% D %*% Vt + store_mean
+  return(X_s)
 }
+
+get_reshape <- function(Xmn,i){
+  smoothed = as.data.frame(t(Xmn))
+  colnames(smoothed) = colnames(Xi[,-1])
+  smoothed$Date <- Xi[,1]
+  pivot_smooth = gather(smoothed, key = "Store", value = "Prediction", -Date)
+  pivot_smooth$Dept = i
+  return(pivot_smooth)
+}
+
 ############## Evaluation Function ############## 
 evaluation = function(){
   test_w_label_path = paste0('Proj2_Data/test_with_label.csv')
@@ -88,27 +114,48 @@ fold_count = 10
 for (fold_num in 1:fold_count) {
   
   file_dir = paste0('fold_', as.character(fold_num))
-
+  predictions = data.frame()
+  
+  if(DEBUG) {
+    print("reached fold ")
+    print(fold_num)
+  } 
+  
   train = read.csv(paste0('Proj2_Data/',file_dir, '/train.csv'))
   test = read.csv(paste0('Proj2_Data/',file_dir, '/test.csv')) 
+  depts = get_depts(train)
+  
+  if(DEBUG) {
+    print("loaded data. Departments are")
+    print(depts)
+  } 
+  
+  for(i in depts){
+    if(DEBUG) {
+      print("department")
+      print(i)
+    } 
+    dept=i
+    Xi = department_matrix(train,dept)
+    X_smoothed = dept_svd(Xi)
+    dept_preds = get_reshape(X_smoothed,dept)
+    full_dept = merge(train, dept_preds, by = c("Store", "Date", "Dept"))
+    predictions = rbind(predictions, full_dept)
+  }
   
   start_last_year = as.Date(min(test$Date)) - 375
   end_last_year = as.Date(max(test$Date)) - 350
   
-  depts = get_depts(train)
-  print(depts)
-  department_matrix(train,1)
-  
-  tmp_train = train %>%
+  tmp_train = predictions %>%
     filter(Date > start_last_year & Date < end_last_year) %>%
     mutate(Wk = ifelse(year(Date) == 2010, week(Date)-1, week(Date))) %>%
-    rename(Weekly_Pred = Weekly_Sales) %>%
+    rename(Weekly_Pred = Prediction) %>%
     select(-Date, -IsHoliday)
   
-  test = test %>%
+  test_wk = test %>%
     mutate(Wk = week(Date))
   
-  test_pred = test %>%
+  test_pred = test_wk %>%
     left_join(tmp_train, by = c('Dept', 'Store', 'Wk')) %>%
     select(-Wk)
   
@@ -134,8 +181,8 @@ for (fold_num in 1:fold_count) {
     print(head(train))
     print("tmp_train")
     print(head(tmp_train))
-    print("test")
-    print(head(test))
+    #print("test")
+    #print(head(test))
     print("test_pred")
     print(head(test_pred))
   } 
--- projects/project2/scratch paper.R
+++ projects/project2/scratch paper.R
@@ -104,3 +104,110 @@ for (i in 1:num_folds) {
 
 
 
+
+
+
+
+
+
+
+
+
+department_matrix = function(train_data, d){
+  matrix = train_data %>%
+    filter(Dept == d) %>%
+    select(Store, Date, Weekly_Sales) %>%
+    spread(Store, Weekly_Sales)
+  
+  if(DEBUG) {
+    print(matrix)
+  } 
+  
+  return(matrix)
+}
+
+get_depts = function(train_data){
+  depts = unique(train_data$Dept)
+  return(depts)
+}
+
+#test_w_label_path = paste0('Proj2_Data/test_with_label.csv')
+#test_w_label = read.csv(test_w_label_path)
+
+train = read.csv(paste0('Proj2_Data/fold_1/train.csv'))
+test = read.csv(paste0('Proj2_Data/fold_1/test.csv')) 
+
+traind=get_depts(train)
+#testd=get_depts(test)
+
+
+depts = get_depts(train)
+print(depts)
+print(length(depts))
+
+predictions = data.frame()
+
+for(i in depts){
+  dept=i
+  Xi = department_matrix(train,dept)
+  X_smoothed = dept_svd(Xi)
+  dept_preds = get_reshape(X_smoothed,dept)
+  full_dept = merge(train, dept_preds, by = c("Store", "Date", "Dept"))
+  predictions = rbind(predictions, full_dept)
+}
+
+predictions %>%
+  arrange(Store, Dept, Date)
+
+Xi = department_matrix(train,77)
+length(Xi)
+nrow(Xi)
+X_smoothed = dept_svd(Xi)
+
+
+start_last_year = as.Date(min(test$Date)) - 375
+end_last_year = as.Date(max(test$Date)) - 350
+
+tmp_train = train %>%
+  filter(Date > start_last_year & Date < end_last_year) %>%
+  mutate(Wk = ifelse(year(Date) == 2010, week(Date)-1, week(Date))) %>%
+  rename(Weekly_Pred = Weekly_Sales) %>%
+  select(-Date, -IsHoliday)
+
+test_wk = test %>%
+  mutate(Wk = week(Date))
+
+test_pred = test_wk %>%
+  left_join(tmp_train, by = c('Dept', 'Store', 'Wk')) %>%
+  select(-Wk)
+
+id = is.na(test_pred$Weekly_Pred)
+test_pred$Weekly_Pred[id] = 0
+
+dept_svd = function(X){
+  m = length(X)
+  n = nrow(X)
+  d = min(m,n)
+  X[is.na(X)] = 0
+  store_mean = rowMeans(t(X[,-1]))
+  X_less_mean = t(X[,-1])-store_mean
+
+  svd_decom = svd(X_less_mean)
+  U = svd_decom$u[,1:d]
+  D = diag(svd_decom$d[1:d])
+  Vt = t(svd_decom$v[,1:d])
+  
+  X_s = U %*% D %*% Vt + store_mean
+  return(X_s)
+}
+
+get_reshape <- function(Xmn,i){
+  smoothed = as.data.frame(t(Xmn))
+  colnames(smoothed) = colnames(Xi[,-1])
+  smoothed$Date <- Xi[,1]
+  pivot_smooth = gather(smoothed, key = "Store", value = "Prediction", -Date)
+  pivot_smooth$Dept = i
+  return(pivot_smooth)
+}
+
+
